8daab8dae7676d4eb724800de7e1a9fac8ac5e9a
Vincent <1779844498@qq.com>
2019-11-30 21:59:04 +0800

    发送邮件的功能

---
+++ module/email.js
@@ -0,0 +1,65 @@
+const nodemailer = require('nodemailer');
+let transporter = require('./sensitiveConfig').transporter;
+
+// 发送登录成功的邮件通知
+exports.sendMailSuccessLogin = function(email, grades) {
+
+    let mailOptions = {
+        from: '"[麦芽糖]成绩通知系统" <1779844498@qq.com>', // sender address
+        to: email, // list of receivers
+        subject: '【登录成功】你已成功登录成绩通知系统', // Subject line
+        html: `<h1>欢迎来到麦芽糖的邮件通知系统</h1><h2>当有新成绩时，我会第一时间通知你哒</h2><p>最新成绩为：</p><p>${grades.className}</p><p>最终成绩：<b>${grades.finalGrade}</b></p><p>期末成绩：<b>${grades.paperGrade}</b></p><p>平时成绩：<b>${grades.regularGrade}</b></p>` // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('发送邮件: %s', info.messageId);
+        // Message sent: <04ec7731-cc68-1ef6-303c-61b0f796b78f@qq.com>
+    });
+
+}
+
+// 发送登录成功但未完成课程评价的邮件通知
+exports.sendMailSuccessLoginWithoutComment = function(email) {
+
+    let mailOptions = {
+        from: '"[麦芽糖]成绩通知系统" <1779844498@qq.com>', // sender address
+        to: email, // list of receivers
+        subject: '【登录成功】你已成功登录成绩通知系统', // Subject line
+        html: `<h1>欢迎来到麦芽糖的邮件通知系统</h1><h2>当有新成绩时，我会第一时间通知你哒</h2><h2>你还没有完成课程评价，我无法查询你的成绩哦</h2>` // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('发送邮件: %s', info.messageId);
+        // Message sent: <04ec7731-cc68-1ef6-303c-61b0f796b78f@qq.com>
+    });
+
+}
+
+//新成绩时,发送邮件
+exports.sendMailNewGrade = function(email,grades) {
+
+    let mailOptions = {
+        from: '"[麦芽糖]成绩通知系统" <1779844498@qq.com>', // sender address
+        to: email, // list of receivers
+        subject: `【新成绩通知】你有新的成绩：${grades.finalGrade}分`, // Subject line
+        html: `<h1>欢迎来到麦芽糖的邮件通知系统</h1><h2>当有新成绩时，我会第一时间通知你哒</h2><p>最新成绩为：</p><p>${grades.className}</p><p>最终成绩：<b>${grades.finalGrade}</b></p><p>期末成绩：<b>${grades.paperGrade}</b></p><p>平时成绩：<b>${grades.regularGrade}</b></p>` // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('发送邮件: %s', info.messageId);
+        // Message sent: <04ec7731-cc68-1ef6-303c-61b0f796b78f@qq.com>
+    });
+
+}
