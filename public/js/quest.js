6a4c89266d274f463b58f56897d22a910a014ea8
VincentTargaryen <1779844498@qq.com>
2019-12-30 02:16:47 +0800

    修复bug

--- public/js/quest.js
+++ public/js/quest.js
@@ -1,13 +1,15 @@
-window.mainApp = new Vue({
-    el: '#mainApp',
-    data: {
-        questData: questData
-    },
-    methods: {
-        handleShowAnsClick(e) {
-            console.log(e.target.getAttribute('data-ans'));
-            e.target.setAttribute('value', e.target.getAttribute('data-ans'))
+(function() {
+    window.mainApp = new Vue({
+        el: '#mainApp',
+        data: {
+            questData: questData
+        },
+        methods: {
+            handleShowAnsClick(e) {
+                console.log(e.target.getAttribute('data-ans'));
+                e.target.setAttribute('value', e.target.getAttribute('data-ans'))
+            }
         }
-    }
-})
-console.log(questData);
+    })
+    console.log(questData);
+})()
