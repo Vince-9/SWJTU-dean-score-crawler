7c71d28bc4dc6c347ce5ffc38bbfc53d254c3aa3
VincentTargaryen <1779844498@qq.com>
2020-01-07 14:18:42 +0800

    延长登录到教务网的延迟

--- module/keepChecking.js
+++ module/keepChecking.js
@@ -14,90 +14,90 @@ let checkingInterval = [];
 let globalUserInfo = [];// 从数据库中查询到的用户信息
 
 function check(userInfo) {
-    setTimeout(() => {
-        //  console.log(userInfo.latest_class_name);
+  setTimeout(() => {
+    //  console.log(userInfo.latest_class_name);
 
-        getLatestGrade.getGradeBySid(userInfo.session_id)
-            .then((grades) => {
-                console.log(new Date(Date.now() + 8 * 60 * 60 * 1000), grades);
-                //若课程名为‘数据’ 说明查询失败 再查
-                if (grades && grades.className != userInfo.latest_class_name && grades.className != '数据') {
-                    logger.log('新成绩！', userInfo.user_name, grades.className);
-                    console.log(`\n\n新成绩！！\n\n`);
-                    //保存新的课程名到数据库
-                    return user.saveClassNameByUserName(userInfo.user_name, grades.className)
-                        .then(() => {
-                            // 发送邮件
-                            email.sendMailNewGrade(userInfo.email, grades, userInfo.email_title_mode);
-                            // 更新内存中的用户数据
-                            exports.reRunAUser(userInfo.user_name);
-                        })
-
-                }
-
-                // 若用户的最新课程名称为空
-                if (!userInfo.className) {
-                    // 保存用户的最新课程名
-                    user.saveClassNameByUserName(userInfo.user_name, grades.className)
-                        .catch((err) => {
-                            console.log(err);
-                        })
-                }
-
-
+    getLatestGrade.getGradeBySid(userInfo.session_id)
+      .then((grades) => {
+        console.log(new Date(Date.now() + 8 * 60 * 60 * 1000), grades);
+        //若课程名为‘数据’ 说明查询失败 再查
+        if (grades && grades.className != userInfo.latest_class_name && grades.className != '数据') {
+          logger.log('新成绩！', userInfo.user_name, grades.className);
+          console.log(`\n\n新成绩！！\n\n`);
+          //保存新的课程名到数据库
+          return user.saveClassNameByUserName(userInfo.user_name, grades.className)
+            .then(() => {
+              // 发送邮件
+              email.sendMailNewGrade(userInfo.email, grades, userInfo.email_title_mode);
+              // 更新内存中的用户数据
+              exports.reRunAUser(userInfo.user_name);
             })
+
+        }
+
+        // 若用户的最新课程名称为空
+        if (!userInfo.className) {
+          // 保存用户的最新课程名
+          user.saveClassNameByUserName(userInfo.user_name, grades.className)
             .catch((err) => {
-                console.log(err);
-                if (err == '未登录') {
-                    login.login2(userInfo.user_name, userInfo.password);
-                }
+              console.log(err);
             })
+        }
 
 
-    }, Math.random() * delayTime * 1000); //避免高并发
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err == '未登录') {
+          login.login2(userInfo.user_name, userInfo.password);
+        }
+      })
+
+
+  }, Math.random() * delayTime * 1000); //避免高并发
 }
 
 exports.runAUser = function (userInfo) {
+  check(userInfo);
+  let temp = setInterval(() => {
     check(userInfo);
-    let temp = setInterval(() => {
-        check(userInfo);
-    }, roundTime * 1000);
+  }, roundTime * 1000);
 
-    checkingInterval.push({
-        interval: temp,
-        userName: userInfo.user_name
-    });
+  checkingInterval.push({
+    interval: temp,
+    userName: userInfo.user_name
+  });
 }
 
 //停止运行
 exports.stopChecking = function () {
-    for (let item of checkingInterval) {
-        clearInterval(item.interval);
-    }
+  for (let item of checkingInterval) {
+    clearInterval(item.interval);
+  }
 }
 
 //开始运行
 exports.startChecking = function () {
-    user.getAllNoShutdownDataFromMySql()
-        .then((results) => {
+  user.getAllNoShutdownDataFromMySql()
+    .then((results) => {
 
-            for (let userInfo of results) {
+      for (let userInfo of results) {
 
-                // console.log(userInfo.latest_class_name);
-                // console.log(userInfo);
-                exports.runAUser(userInfo);
+        // console.log(userInfo.latest_class_name);
+        // console.log(userInfo);
+        exports.runAUser(userInfo);
 
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 }
 
 //重新运行
 exports.reRun = function () {
-    exports.stopChecking();
-    exports.startChecking();
+  exports.stopChecking();
+  exports.startChecking();
 }
 
 /**
@@ -105,37 +105,37 @@ exports.reRun = function () {
  * @param
  */
 exports.updateUserInfoInMeo = function () {
-    user.getAllNoShutdownDataFromMySql()
-        .then(result => {
-            globalUserInfo = result;
-        })
+  user.getAllNoShutdownDataFromMySql()
+    .then(result => {
+      globalUserInfo = result;
+    })
 }
 
 /**
  * 停止一名用户的执行
  */
 exports.stopAUser = function (userName) {
-    checkingInterval.forEach((item, index) => {
-        if (item.userName == userName) {
-            clearInterval(item.interval);
-            checkingInterval.splice(index, 1);
-        }
-    })
+  checkingInterval.forEach((item, index) => {
+    if (item.userName == userName) {
+      clearInterval(item.interval);
+      checkingInterval.splice(index, 1);
+    }
+  })
 }
 
 /**
  * 更新内存中一名用户的信息，并重新运行它
  */
 exports.reRunAUser = function (userName) {
-    exports.stopAUser(userName);
-    user.findUserByName(userName)
-        .then(res => {
-            if (res && res[0] && res[0].status == 1)
-                exports.runAUser(res[0]);
-        })
-        .catch(err => {
-            console.log(err);
-        })
+  exports.stopAUser(userName);
+  user.findUserByName(userName)
+    .then(res => {
+      if (res && res[0] && res[0].status == 1)
+        exports.runAUser(res[0]);
+    })
+    .catch(err => {
+      console.log(err);
+    })
 }
 
 //exports.startChecking();//开始运行
