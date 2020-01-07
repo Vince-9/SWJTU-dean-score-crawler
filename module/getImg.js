2294d5d857a5573fffb50adea1695e13a8d8de6d
VincentTargaryen <1779844498@qq.com>
2020-01-07 15:34:04 +0800

    修复bug：获取验证码图片超时时的错误

--- module/getImg.js
+++ module/getImg.js
@@ -8,40 +8,41 @@ let sid = '';
 
 //参数:学号
 exports.getImgAndSession = function (userName) {
-    return new Promise(function (_resolve, _reject) {
-        request({
-            url: imgUrl,
-            encoding: 'binary'
-        }, (err, res, body) => {
-            //获取session id 
-            sid = res.caseless.dict['set-cookie'][0].split(';')[0];
-            _resolve([body, sid]);//body是图片
-        });
-    })
-        .then(([body, sid]) => {
-            let writeFilePromise = new Promise((resolve, reject) => {
-                fs.writeFile(`./public/data/randImg/${userName}.jpg`, body, 'binary', (err) => {
-                    if (err) {
-                        reject('验证码图片写入失败');
-                        console.log(err);
-                    }
-                    resolve();
-                });//写入照片到文件
-            })
+	return new Promise(function (_resolve, _reject) {
+		request({
+			url: imgUrl,
+			encoding: 'binary'
+		}, (err, res, body) => {
+			//获取session id 
+			sid = res && res.caseless.dict['set-cookie'][0].split(';')[0];
+			sid && _resolve([body, sid]);//body是图片
+			sid || reject('获取img失败');
+		});
+	})
+		.then(([body, sid]) => {
+			let writeFilePromise = new Promise((resolve, reject) => {
+				fs.writeFile(`./public/data/randImg/${userName}.jpg`, body, 'binary', (err) => {
+					if (err) {
+						reject('验证码图片写入失败');
+						console.log(err);
+					}
+					resolve();
+				});//写入照片到文件
+			})
 
-            //将sid存到数据库
-            let saveSidPromise = User.saveSid(userName, sid);
+			//将sid存到数据库
+			let saveSidPromise = User.saveSid(userName, sid);
 
-            return Promise.all([writeFilePromise, saveSidPromise])
-                .then(() => {
-                    console.log(`图片写入成功,sid保存成功,学号${userName}, sid: ${sid}`);
-                    return sid;
-                })
+			return Promise.all([writeFilePromise, saveSidPromise])
+				.then(() => {
+					console.log(`图片写入成功,sid保存成功,学号${userName}, sid: ${sid}`);
+					return sid;
+				})
 
 
-        })
-        .catch(err => {
-            console.log(err);
-        })
+		})
+		.catch(err => {
+			console.log(err);
+		})
 }
 
