b907ed0b61c4aa61274540b81998784e237ca1a4
VincentTargaryen <1779844498@qq.com>
2020-01-12 13:17:05 +0800

    修改邮件内容

--- module/email.js
+++ module/email.js
@@ -58,7 +58,8 @@ exports.sendMailNewGrade = function (email, grades, mode) {
 			from: `"[Vincent] GPA" `, // sender address
 			to: email, // list of receivers
 			subject: emailTitle, // Subject line
-			html: `<h1>Welcome to Vincent's email notification system</h1><h2>Tomorrow and grades, you never know which comes first.</h2><h2>抱歉，这封邮件可能迟到了数小时，因为我写的bug导致系统挂了</h2><p>subject:</p><p>${grades.className}</p><p>final grades:<b>${grades.finalGrade}</b></p><p>paper grades：<b>${grades.paperGrade}</b></p><p>regular grades:<b>${grades.regularGrade}</b></p>`
+			html: `<h1>Welcome to Vincent's email notification system</h1><h2>Tomorrow and grades, you never know which comes first.
+			</h2><p>subject:</p><p>${grades.className}</p><p>final grades:<b>${grades.finalGrade}</b></p><p>paper grades：<b>${grades.paperGrade}</b></p><p>regular grades:<b>${grades.regularGrade}</b></p>`
 			//验证码：${Math.ceil(Math.random() * 10000)} 这个验证码是为了反垃圾邮件的，并没有什么用`
 		};
 	} else {
@@ -70,7 +71,7 @@ exports.sendMailNewGrade = function (email, grades, mode) {
 			//<h2>明天和成绩，你永远不知道哪一个先来。</h2>
 			// html: ``
 			html: `<h1>欢迎来到麦芽糖的邮件通知系统</h1>
-			<h2>抱歉，这封邮件可能迟到了数小时，因为我写的bug导致系统挂了</h2>
+			<h2>明天和成绩，你永远不知道哪一个先来。</h2>
 			<p>最新成绩为：</p><p>${grades.className}</p><p>最终成绩：<b>${grades.finalGrade}</b></p><p>期末成绩：<b>${grades.paperGrade}</b></p><p>平时成绩：<b>${grades.regularGrade}</b></p>
 			      验证码：${Math.ceil(Math.random() * 10000)} 这个验证码是为了反垃圾邮件的，并没有什么用`
 			// html: `<h1>非常抱歉，今天这边出现了一些状况，导致今天的通知出现了数小时的延迟</h1><h2>明天和成绩，你永远不知道哪一个先来。</h2><p>最新成绩为：</p><p>${grades.className}</p><p>最终成绩：<b>${grades.finalGrade}</b></p><p>期末成绩：<b>${grades.paperGrade}</b></p><p>平时成绩：<b>${grades.regularGrade}</b></p>
